1d515ca1bf0f28db8b7557c3eb703820c116502f
Oscar Frischmann <oscarfrisc@gmail.com>
2023-09-11 15:35:51 -0300

    flags added

--- js/footer.js
+++ js/footer.js
@@ -1,3 +1,16 @@
+const navTexts = ['Nosotros', 'La Hostería', 'Contacto', 'Ubicación', 'Fotos'];
+
+//* HEADER
+const headerNavs = document.querySelectorAll('.header__navs-pages li a');
+console.log(headerNavs)
+headerNavs.forEach((e, i) => {
+    e.textContent = navTexts[i];
+})
+
+
+
+
+//* FOOTER
 const adress = document.querySelector('.footer__adress--index');
 adress.innerHTML = "Glaciar Río Túnel 210 <br>El Calafate <br>Santa Cruz <br>República\nArgentina";
 
@@ -6,6 +19,5 @@ console.log(footerNavs)
 
 
 footerNavs.forEach((e, i) => {
-    const footerNavTexts = ['Nosotros', 'La Hostería', 'Contacto', 'Ubicación', 'Fotos'];
-    e.textContent = footerNavTexts[i]
+    e.textContent = navTexts[i];
 })
